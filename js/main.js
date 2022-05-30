795ec67519170d6e7cecd35d057840dd47b8bed6
Ahadi Habib Josue <51280463+ahadihjosue@users.noreply.github.com>
2022-05-30 18:35:29 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function getFeedback() {
+    var fname = document.getElementById('fname').value;
+    var surname = document.getElementById('surname').value;
+    var email = document.getElementById('email').value;
+    var date = document.getElementById('date').value;
+    var memories = document.getElementById('memories').value;
+
+    alert(+surname+", Thank you for sharing your memories :)")
+}
